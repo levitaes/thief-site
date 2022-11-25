cc1f5ccde49950985d60b906053b27c8c24c3b11
Eric Boschmann <e.boschmann@meddevo.com>
2022-11-25 20:46:25 +0100

    feature: add half baked internet

--- code/winbox.apps.js
+++ code/winbox.apps.js
@@ -4,7 +4,18 @@ function terminalApp () {
         x: "center",
         y: "center",
         width: "50%",
-        height: "50%",
+        height: "40%",
         url: "https://thief.lerchen.net/OS.js/",
+    });
+}
+
+function internetApp () {
+    new WinBox({
+        title: "Internet",
+        x: "center",
+        y: "center",
+        width: "65%",
+        height: "50%",
+        url: "apps/internet.app.html",
     });
 }
